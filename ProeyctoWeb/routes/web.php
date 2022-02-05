546e4cfaf4a5ea873fa1001a7e4ca80824db631b
lusdolot01 <lusdolot@gmail.com>
2022-02-04 20:08:52 -0600

    Codigo que si enlaza

---
+++ ProeyctoWeb/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\HomeController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/saludo',function() {
+    return 'Hola a todos';
+});
+
+Route::get('/bienvenida/{name?}','App\Http\Controllers\HomeController@index');
